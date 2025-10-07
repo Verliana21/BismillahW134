799e00388f6836d03f491cf12cf01edca6664a5c
Verliana21 <5002221076@student.its.ac.id>
2025-10-07 20:54:12 +0700

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,3 +1,3 @@
-python inference_one_sample.py --image demo/src/COD_CAMO_camourflage_00012.jpg \
-                               --mask demo/src/COD_CAMO_camourflage_00012.png \
+python inference_one_sample.py --image /kaggle/input/military-camouflage-soldiers-dataset-mcs1k/dataset-splitM/Training/images/image1.jpg \
+                               --mask /kaggle/input/military-camouflage-soldiers-dataset-mcs1k/dataset-splitM/Training/GT/image1.jpg \
                                --log_path demo_res 
